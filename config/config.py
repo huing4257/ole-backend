02f702deacad4affe507ba4fab1841c7c6da9f1e
WangShengyuan <administrator@DESKTOP-795IE77>
2023-03-24 23:17:42 +0800

    :sparkles:feat add the first version of login and logout

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testdb',
-        'USER': 'testuser',
-        'PASSWORD': 'testpwd',
-        'HOST': 'mysql',
-        'PORT': 3306,
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3'
     }
 }
