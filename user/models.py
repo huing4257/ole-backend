7372f544b2b931a18255071f0ccfae1eee9f38a1
huing4257 <huing4257@163.com>
2023-03-24 17:18:24 +0800

    :sparkles: feat: add user app and User model ole-web#2

---
+++ user/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    user_id = models.BigIntegerField(primary_key=True)
+    user_name = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    user_type = models.CharField(max_length=20)
+    score = models.IntegerField()
+    membership_level = models.IntegerField()
+    invite_code = models.CharField(max_length=20)
+
+    class Meta:
+        indexes = [models.Index(fields=["user_name"])]
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "user_name": self.user_name,
+            "user_type": self.user_type,
+            "score": self.score,
+            "membership_level": self.membership_level,
+            "invite_code": self.invite_code
+        }
+
+    def __str__(self) -> str:
+        return self.user_name
