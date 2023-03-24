77f9f4dc02cdce6ae40867a5d508f768eda278ec
袁辰飞 <2021010511@secoder.net>
2023-03-24 15:58:41 +0800

    Initial commit

---
+++ config/config.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': 'testpwd',
+        'HOST': 'mysql',
+        'PORT': 3306,
+    }
+}
