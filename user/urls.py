0d89ef5e06e239e7d627787a37ca6788d3c3f0eb
胡赢 <2021012803@secoder.net>
2023-03-25 12:44:43 +0800

    :twisted_rightwards_arrows: fix:deal with differences between branches of register and login ole-web#2

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ import user.views as views
 urlpatterns = [
     path('register', views.register),
     path('login', views.login),
+    path('logout', views.logout),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import HttpRequest
-from utils.utils_request import request_failed, request_success, return_field
+from utils.utils_request import request_failed, request_success, return_field, BAD_METHOD
 from utils.utils_require import require
 from user.models import User
 import bcrypt
@@ -29,4 +29,39 @@ def register(req: HttpRequest):
 
 
 def login(req: HttpRequest):
+    if req.method == "POST":
+        # 通过cookie判断是否已经登录
+        if "userId" in req.COOKIES and "userName" in req.COOKIES and "userType" in req.COOKIES:
+            return_data = {
+                "user_id": req.COOKIES["userId"],
+                "user_name": req.COOKIES["userName"],
+                "user_type": req.COOKIES["userType"],
+            }
+            return request_success(return_data)
+
+        body = json.loads(req.body.decode("utf-8"))
+        uname = require(body, "user_name", "string", err_msg="Missing or error type of name")
+        upassword = require(body, "password", "string", err_msg="Missing or error type of password")
+        user: User = User.objects.filter(user_name=uname)
+        if not user:
+            return request_failed(4, "wrong username or password", 400)
+        else:
+            if bcrypt.hashpw(upassword, bcrypt.gensalt()) == user.password:
+                return_data = {
+                    "user_id": user.user_id,
+                    "user_name": user.user_name,
+                    "user_type": user.user_type,
+                }
+                response = request_success(return_data)
+                response.set_cookie("userId", user.user_id)
+                response.set_cookie("userName", user.user_name)
+                response.set_cookie("userType", user.user_type)
+                return response
+            else:
+                return request_failed(4, "wrong username or password", 400)
+    else:
+        return BAD_METHOD
+
+
+def logout(req: HttpRequest):
     return None
