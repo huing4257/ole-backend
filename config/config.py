a51fbcae05192ef934b677aeec527578bbeed3ef
WangShengyuan <administrator@DESKTOP-795IE77>
2023-03-26 17:53:27 +0800

    :construction: fix error(ValueError: The database backend does not accept 0 as a value for AutoField.) ole-web#2

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testdb',
-        'USER': 'testuser',
+        'USER': 'root',
         'PASSWORD': 'testpwd',
         'HOST': 'mysql',
         'PORT': 3306,
