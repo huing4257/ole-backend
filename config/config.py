9626ed681f6bc3eb0d31064a00c2b24f0d7de7c8
WangShengyuan <administrator@DESKTOP-795IE77>
2023-03-26 17:04:33 +0800

    :construction: fix error(ValueError: The database backend does not accept 0 as a value for AutoField.)

--- config/config.py
+++ config/config.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': 'testpwd',
+        'HOST': 'mysql',
+        'PORT': 3306,
     }
 }
