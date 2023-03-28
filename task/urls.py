b73b10705b59bd942e2d4e9e6227ee2eca252dc9
胡赢 <2021012803@secoder.net>
2023-03-28 20:18:30 +0800

    :sparkles: feat: add func for tasks and modify urls ole-web#4

--- task/urls.py
+++ task/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 import task.views as views
 
 urlpatterns = [
-    path('/', views.task_init),
-    path('/<task_id>', views.task_ops),
+    path('', views.create_task),
+    path('<task_id>', views.task_ops),
     path('all', views.all),
     path('get_my_tasks', views.get_my_tasks),
     path('upload_data/<task_id>', views.upload_data),
--- task/views.py
+++ task/views.py
@@ -1,20 +1,60 @@
 import json
 from django.http import HttpRequest
-from utils.utils_request import request_failed, request_success, BAD_METHOD, return_field
+
+from utils.utils_check import CheckLogin
+from utils.utils_request import request_failed, request_success, BAD_METHOD
 from utils.utils_require import require, CheckRequire
-from utils.utils_time import get_timestamp
 from user.models import User, UserToken
 from task.models import Task
-import bcrypt
 
 
 # Create your views here.
-def task_init():
-    pass
+@CheckRequire
+def require_tasks(req):
+    body = json.loads(req.body.decode("utf-8"))
+    task_type = require(body, "task_type", "string", err_msg="Missing or error type of [taskType]")
+    task_style = require(body, "task_style", "string", err_msg="Missing or error type of [taskStyle]")
+    reward_per_q = require(body, "reward_per_q", "int", err_msg="time limit or reward score format error",
+                           err_code=9)
+    time_limit_per_q = require(body, "time_limit_per_q", "int", err_msg="time limit or reward score format error",
+                               err_code=9)
+    total_time_limit = require(body, "total_time_limit", "int", err_msg="time limit or reward score format error",
+                               err_code=9)
+    auto_ac = require(body, "auto_accept", "bool", err_msg="auto accept format error")
+    manual_ac = require(body, "manual_accept", "bool", err_msg="manual accept format error")
+    return {"task_type": task_type, "task_style": task_style, "reward_per_q": reward_per_q,
+            "time_limit_per_q": time_limit_per_q, "total_time_limit": total_time_limit,
+            "auto_ac": auto_ac, "manual_ac": manual_ac}
 
 
-def task_ops():
-    pass
+@CheckLogin
+def create_task(req: HttpRequest, user: User):
+    if req.method == 'POST':
+        para = require_tasks(req)
+        para.update({"publisher": user})
+        task = Task(**para)
+        task.save()
+        return request_success({"task_id": task.task_id})
+    else:
+        return BAD_METHOD
+
+
+@CheckLogin
+@CheckRequire
+def task_ops(req: HttpRequest, user: User, task_id: any):
+    task_id = require({"task_id": task_id}, "task_id", "int", err_msg="Bad param [task_id]", err_code=-1)
+    if req.method == 'PUT':
+        task = Task.objects.filter(task_id=task_id).first()
+        if task is None:
+            return request_failed(1, "Task not found", status_code=404)
+        elif task.publisher != user:
+            return request_failed(1, "You are not the publisher of this task", status_code=403)
+        else:
+            para = require_tasks(req)
+            for key in para:
+                setattr(task, key, para[key])
+            task.save()
+            return request_success({"task_id": task.task_id})
 
 
 def all():
@@ -36,7 +76,7 @@ def get_my_tasks(req: HttpRequest):
                 distributed = user.distributed_tasks.all()
                 distributed_list: list = list()
                 for element in distributed:
-                    published_list.append(element.serialize())
+                    user.published_list.append(element.serialize())
                 return request_success(distributed_list)
             else:
                 return request_failed(12, "no task of admin")
