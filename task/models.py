322cb825ee883d5db0f00285f3aec36584745f93
WangShengyuan <2021010764@secoder.net>
2023-04-03 21:51:59 +0800

    :construction: feat: rewrite Task/models to docs ole-web#4

--- task/models.py
+++ task/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
-
+from user.models import User
 # Create your models here.
+
+
 class Data(models.Model):
     data = models.JSONField()
 
@@ -12,14 +13,31 @@ class Data(models.Model):
 
 
 class Result(models.Model):
-    user_id = models.IntegerField()
-    result = models.JSONField()
+    tag_user = models.ManyToManyField(User)
+    tag_res = models.CharField()
 
     def serialize(self):
         return {
-            "user_id": self.user_id,
-            "result": self.result
+            "tag_user": self.tag_user,
+            "tag_res": self.tag_res
         }
+
+
+class Question(models.Model):
+    data = models.CharField()
+    result = models.ManyToManyField(Result)
+    # 文字/图片/视频
+    data_type = models.CharField()
+
+
+class Current_tag_user(models.Model):
+    tag_user = models.ManyToManyField(User)
+    accepted_at = models.IntegerField()
+
+
+class Progress(models.Model):
+    tag_user = models.ManyToManyField(User)
+    q_id = models.IntegerField()
 
 
 class Task(models.Model):
@@ -31,12 +49,15 @@ class Task(models.Model):
     auto_ac = models.BooleanField(default=True)
     manual_ac = models.BooleanField(default=False)
     publisher = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name="published_task", null=True)
-    data = models.ManyToManyField('task.Data', related_name="task", blank=True)
-    distribute_users = models.ManyToManyField('user.User', related_name="distributed_tasks")
     task_id = models.AutoField(primary_key=True)
     distribute_user_num = models.IntegerField(default=0)
-    result = models.ManyToManyField('task.Result')
+    q_num = models.IntegerField(default=0)
     task_name = models.CharField(max_length=24, default="task")
+    questions = models.ManyToManyField(Question)
+    current_tag_user_list = models.ManyToManyField(Current_tag_user)
+    past_tag_user_list = models.ManyToManyField(User)
+    progress = models.ManyToManyField(Progress)
+    result_type = models.CharField()
 
     def serialize(self):
         return {
@@ -56,28 +77,3 @@ class Task(models.Model):
             "task_name": self.task_name,
         }
 
-
-class Question(models.Model):
-    # 1-index
-    q_id = models.AutoField()
-    q_description = models.CharField()
-    data = models.ManyToManyField()
-    result = models.CharField()
-    # 文字/图片/视频
-    data_type = models.CharField()
-
-
-class SubTask(models.Model):
-    subtask_id = models.IntegerField(primary_key=True, unique=True)
-    # 问题信息列表
-    questions = models.ManyToManyField()
-    # 问题数目 子任务中包含的问题数目
-    q_num = models.IntegerField()
-    # 子任务是否已被分发
-    distributed = models.BooleanField()
-    # 被分发者的user_id
-    tag_user = models.IntegerField()
-    # 任务被标注方接受时间 默认值为0，重新分发时需要设置为0
-    accepted_at = models.IntegerField(default=0)
-    # 被分发用户id列表 用于确保重新分发时不重复分发给同一个用户
-    tag_user_list = models.ManyToManyField()
