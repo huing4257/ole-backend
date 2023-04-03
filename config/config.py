1c08462dffe305558f2b91a6dfb30fb47e18eabc
WangShengyuan <2021010764@secoder.net>
2023-04-03 22:15:41 +0800

    :construction: feat: rewrite Task/models to docs ole-web#4

--- config/config.py
+++ config/config.py
@@ -1,22 +1,22 @@
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'testdb',
-#         'USER': 'root',
-#         'PASSWORD': 'testrootpwd',
-#         'HOST': 'mysql',
-#         'PORT': 3306,
-#     }
-# }
-# 本地调试用
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': 'testrootpwd',
+        'HOST': 'mysql',
+        'PORT': 3306,
     }
 }
+# 本地调试用
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3'
+#     }
+# }
