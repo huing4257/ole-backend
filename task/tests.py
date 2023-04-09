3dca6dcc10ff39d0846f8b0bf55fcec360c978bf
WangShengyuan <2021010764@secoder.net>
2023-04-09 22:13:07 +0800

    :construction: fix: potential cause of test bugs ole-web#4

--- task/tests.py
+++ task/tests.py
@@ -60,7 +60,7 @@ class TaskTests(TestCase):
         )
 
         task = Task.objects.create(
-            task_type="",
+            task_type="text",
             task_style="",
             reward_per_q=0,
             time_limit_per_q=1,
@@ -77,7 +77,7 @@ class TaskTests(TestCase):
         )
         question2 = Question.objects.create(
             q_id=2,
-            data="1",
+            data="2",
             data_type="text",
         )
 
@@ -338,13 +338,13 @@ class TaskTests(TestCase):
     #     self.assertEqual(res.json()["message"], "Succeed")
     #     self.assertEqual(res.json()["data"], Question.objects.get(q_id=1).serialize(detail=True))
 
-    # def test_get_task_question_receiver(self):
-    #     self.client.post("/user/login", {"user_name": "testReceiver1", "password": "testPassword"},
-    #                      content_type=default_content_type)
-    #     res = self.client.get(f"/task/{1}/{2}")
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(res.json()["message"], "Succeed")
-    #     self.assertEqual(res.json()["data"], Question.objects.get(q_id=2).serialize(detail=True))
+    def test_get_task_question_receiver(self):
+        self.client.post("/user/login", {"user_name": "testReceiver1", "password": "testPassword"},
+                         content_type=default_content_type)
+        res = self.client.get(f"/task/{1}/{2}")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json()["message"], "Succeed")
+        self.assertEqual(res.json()["data"], Question.objects.get(q_id=2).serialize(detail=True))
 
     def test_get_task_question_not_receiver(self):
         self.client.post("/user/login", {"user_name": "testReceiver2", "password": "testPassword"},
