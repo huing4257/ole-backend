1aff03b93b54bfd00b9cdf81d82d98b6e1b7ab5c
胡赢 <2021012803@secoder.net>
2023-04-19 20:59:09 +0800

    :white_check_mark: test: add test for upload ans and review ole-web#8

--- review/tests.py
+++ review/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 import bcrypt
 from user.models import User
-from task.models import Task
+from review.models import AnsList
+from django.core.files.uploadedfile import SimpleUploadedFile
 import datetime
 
 default_content_type = "application/json"
@@ -56,12 +57,18 @@ class ReviewTests(TestCase):
         }, content_type=default_content_type)
         return response
 
-    def test_manual_check_success(self):
+    def login_create_task(self, distribute_user_num=1):
         res = self.login("testPublisher")
         self.assertEqual(res.status_code, 200)
-        res = self.client.post("/task/", self.para, content_type=default_content_type)
+        para = self.para.copy()
+        para["distribute_user_num"] = distribute_user_num
+        res = self.client.post("/task/", para, content_type=default_content_type)
         self.assertEqual(res.status_code, 200)
         task_id = res.json()["data"]["task_id"]
+        return task_id
+
+    def test_manual_check_success(self):
+        task_id = self.login_create_task()
         res = self.client.post(f"/task/distribute/{task_id}")
         self.assertEqual(res.status_code, 200)
 
@@ -69,7 +76,6 @@ class ReviewTests(TestCase):
         res = self.login("testReceiver1")
         self.assertEqual(res.status_code, 200)
         res = self.client.post(f"/task/accept/{task_id}")
-        js = res.json()
         self.assertEqual(res.status_code, 200)
         res = self.client.post(f"/task/upload_res/{task_id}/1", {
             "result": "tag_1"
@@ -85,3 +91,45 @@ class ReviewTests(TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json()["data"]["q_info"][0]["result"]["tag_res"], "tag_1")
 
+    def test_manual_check_no_permission(self):
+        self.login("testPublisher")
+        res = self.client.post("/task/", self.para, content_type=default_content_type)
+        task_id = res.json()["data"]["task_id"]
+
+        self.client.post("/user/logout")
+        self.login("testReceiver1")
+
+        res = self.client.post(f"/review/manual_check/{task_id}/2", {"check_method": "select"},
+                               content_type=default_content_type)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json()["code"], 16)
+
+    def test_upload_stdans_success(self):
+        task_id = self.login_create_task()
+
+        with open("a.csv", "w") as f:
+            f.write("1.txt,tag_1_1")
+        file = SimpleUploadedFile("a.csv", open("a.csv", "rb").read())
+        res = self.client.post(f"/review/upload_stdans", {
+            "file": file
+        })
+        ans_id = res.json()["data"]
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(AnsList.objects.filter(id=int(ans_id)).exists())
+
+    def test_review_accept(self):
+        task_id = self.login_create_task()
+        self.client.post(f"/task/distribute/{task_id}")
+        res = self.client.post(f"/review/accept/{task_id}/2")
+        self.assertEqual(res.status_code, 200)
+
+    def test_review_acc_not_distributed(self):
+        task_id = self.login_create_task()
+        res = self.client.post(f"/review/accept/{task_id}/2")
+        self.assertEqual(res.status_code, 400)
+
+    def test_review_reject(self):
+        task_id = self.login_create_task()
+        self.client.post(f"/task/distribute/{task_id}")
+        res = self.client.post(f"/review/accept/{task_id}/2")
+        self.assertEqual(res.status_code, 200)
