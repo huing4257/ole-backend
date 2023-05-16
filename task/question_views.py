3cf02929baf281c957b7088497c19ed906826b1b
袁辰飞 <2021010511@secoder.net>
2023-05-16 13:16:05 +0800

    :bug: fix: fix update task state when upload res ole-web#61

--- task/question_views.py
+++ task/question_views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http import HttpRequest
 
-from task.models import Task, Result, Question, Progress, CurrentTagUser, InputResult, InputType
+from task.models import Task, Result, Question, CurrentTagUser, InputResult, InputType
 from user.models import User
 from user.vip_views import add_grow_value
 from utils.utils_check import CheckLogin, CheckUser
@@ -52,48 +52,29 @@ def upload_res(req: HttpRequest, user: User, task_id: int, q_id: int):
         quest.save()
         task.save()
 
-        # 处理progress
-        progress: Progress = task.progress.filter(tag_user=user).first()
-        if progress:
-            # 已经做过这个题目
-            # 判断是最后一道题
-            if q_id < task.q_num:
-                progress.q_id = q_id + 1
-            else:
-                if all(q.result.filter(tag_user=user).exists() for q in task.questions.all()):
-                    # 所有题目已经做完了，就把progress设为0
-                    progress.q_id = 0
-                    curr_tag_user: CurrentTagUser = task.current_tag_user_list.filter(tag_user=user).first()
-                    curr_tag_user.state = "finished"
+        # 如果所有题目都做完了
+        if all(q.result.filter(tag_user=user).exists() for q in task.questions.all()):
+            curr_tag_user: CurrentTagUser = task.current_tag_user_list.filter(tag_user=user).first()
+            curr_tag_user.state = "finished"
 
-                    # 开始自动审核
-                    if task.accept_method == "auto":
-                        curr_tag_user.state = "check_accepted"
-                        ans_list = task.ans_list
-                        questions = task.questions.all()
-                        for ans in ans_list.ans_list.all():
-                            q_id = int(ans.filename.split('.')[0])
-                            question = questions.filter(q_id=q_id).first()
-                            result = question.input_res.all().filter(tag_user=user).first()
-                            if result.tag_res != ans.std_ans:
-                                curr_tag_user.state = "check_refused"
-                        if curr_tag_user.state == "check_accepted":
-                            # 给标注方加分
-                            user.score += task.reward_per_q * task.q_num
-                            user.tag_score += task.reward_per_q * task.q_num
-                            add_grow_value(user, 10)
-                            user.save()
-                    curr_tag_user.save()
-                else:
-                    progress.q_id = q_id
-            progress.save()
-        else:
-            # 这个用户还没做过这个题目，创建
-            progress: Progress = Progress.objects.create(
-                tag_user=user,
-                q_id=q_id + 1,
-            )
-            task.progress.add(progress)
+            # 开始自动审核
+            if task.accept_method == "auto":
+                curr_tag_user.state = "check_accepted"
+                ans_list = task.ans_list
+                questions = task.questions.all()
+                for ans in ans_list.ans_list.all():
+                    q_id = int(ans.filename.split('.')[0])
+                    question = questions.filter(q_id=q_id).first()
+                    result = question.input_res.all().filter(tag_user=user).first()
+                    if result.tag_res != ans.std_ans:
+                        curr_tag_user.state = "check_refused"
+                if curr_tag_user.state == "check_accepted":
+                    # 给标注方加分
+                    user.score += task.reward_per_q * task.q_num
+                    user.tag_score += task.reward_per_q * task.q_num
+                    add_grow_value(user, 10)
+                    user.save()
+            curr_tag_user.save()
         task.save()
         return request_success()
     else:
