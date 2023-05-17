7125089c73a4998d44c59d3bc84bee4b7f43550c
WangShengyuan <2021010764@secoder.net>
2023-05-17 10:50:44 +0800

    :white_check_mark: test: add test to task/accept_task ole-web#65

--- task/tests.py
+++ task/tests.py
@@ -684,6 +684,58 @@ class TaskTests(TestCase):
         self.assertEqual(res.status_code, 400)
         self.assertEqual(res.json()["message"], "no permission to accept")
 
+    def test_accept_task(self):
+        self.client.post("/user/login", {"user_name": "testPublisher", "password": "testPassword"},
+                         content_type=default_content_type)        
+        para = self.para.copy()
+        para["distribute_user_num"] = 3
+        res = self.client.post("/task/", para, content_type=default_content_type)
+        self.assertEqual(res.status_code, 200)
+        task_id = res.json()['data']['task_id']
+
+        set_task_checked(task_id)
+        res = self.client.post(f"/task/distribute/{task_id}")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json()["message"], "Succeed")
+        task = Task.objects.get(task_id=task_id)
+
+        distribute_user_list = set(tag_user.tag_user.user_id for tag_user in task.current_tag_user_list.all())
+        self.assertEqual(len(distribute_user_list), 3)
+        self.client.post("/user/logout")
+        self.client.post("/user/login", {"user_name": "testReceiver1", "password": "testPassword"},
+                         content_type=default_content_type)
+        res = self.client.post(f"/task/accept/{task_id}")
+        self.assertEqual(res.status_code, 200)        
+
+    def test_accept_task_strategy_toall(self):
+        self.client.post("/user/login", {"user_name": "testPublisher", "password": "testPassword"},
+                         content_type=default_content_type)         
+        para = self.para.copy()
+        para["distribute_user_num"] = 3
+        para["strategy"] = "toall"
+        res = self.client.post("/task/", para, content_type=default_content_type)
+        self.assertEqual(res.status_code, 200)
+        task_id = res.json()['data']['task_id']
+        set_task_checked(task_id)
+        res = self.client.post(f"/task/distribute/{task_id}")
+        self.assertEqual(res.status_code, 400)
+        self.assertJSONEqual(
+            res.content, {"code": 22, "message": "task has been distributed", "data": {}}
+        )
+
+        self.client.post("/user/logout")
+        self.client.post("/user/login", {"user_name": "testReceiver1", "password": "testPassword"},
+                         content_type=default_content_type)
+        res = self.client.post(f"/task/accept/{task_id}")
+        self.assertEqual(res.status_code, 200)   
+        res = self.client.post(f"/task/accept/{task_id}")
+        self.assertEqual(res.status_code, 200)   
+        res = self.client.post(f"/task/accept/{task_id}")
+        self.assertEqual(res.status_code, 400) 
+        self.assertJSONEqual(
+            res.content, {"code": 32, "message": "repeat accept", "data": {}}
+        )          
+
     def test_accept_task_no_permission(self):
         self.client.post("/user/login", {"user_name": "testReceiver2", "password": "testPassword"},
                          content_type=default_content_type)
